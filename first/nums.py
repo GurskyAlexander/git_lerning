c24368b795dc73714fb8e0e8f06615e2b3e7f3a7
Admin <filigrando2@yandex.ru>
2023-10-16 12:28:54 +1100

    Добавлена функция имнус

--- first/nums.py
+++ first/nums.py
@@ -2,5 +2,11 @@ def plus(a: int, b: int) -> int:
     return a + b
 
 
+text = 'new text'
+
+def minus(a, b):
+    return a - b
+
 if __name__ == '__main__':
     print(plus(2, 2))
+    print(minus(4, 2))
